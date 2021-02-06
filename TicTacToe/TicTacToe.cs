bea5f20228558d0a301ecee846cf7bec40098981
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-06 11:56:06 +0530

    [Dipesh] modify ChoseCharacter

--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -7,6 +7,8 @@ namespace TicTacToe
     class TicTacToe
     {
         char[] gameBoard;
+        public char playerCharacter;
+        public char computerCharacter;
         public TicTacToe()
         {
             gameBoard = new char[10];
@@ -15,6 +17,37 @@ namespace TicTacToe
                 gameBoard[i] = '-';
             }
         }
-
+        public void DisplayBoard()
+        {
+            Console.WriteLine("-------------");
+            for (int i = 1; i < 10; i++)
+            {
+                Console.Write($" {gameBoard[i]} ");
+                if (i%3==0)
+                {
+                    Console.WriteLine("");
+                }
+            }
+            Console.WriteLine("-------------");
+        }
+        public void ChooseLetter()
+        {
+            Console.WriteLine("Choose Between X and O, Type your Choice");
+            char inputChar = Console.ReadLine()[0];
+            if (inputChar == 'X')
+            {
+                playerCharacter = inputChar;
+                computerCharacter = 'O';
+            }
+            else if (inputChar == 'O')
+            {
+                playerCharacter = inputChar;
+                computerCharacter = 'X';
+            }
+            else
+            {
+                Console.WriteLine("Choose Proper Character");
+            }
+        }
     }
 }
