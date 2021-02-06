679ecec8a12dad29c73886e7b050ea607f3518a7
Dipesh Walte <dipesh4edutopia@users.noreply.github.com>
2021-02-06 11:18:10 +0530

    Add project files.

---
+++ TicTacToe/TicTacToe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class TicTacToe
+    {
+        char[] gameBoard;
+        public TicTacToe()
+        {
+            gameBoard = new char[10];
+            for (int i = 1; i < 10; i++)
+            {
+                gameBoard[i] = '-';
+            }
+        }
+
+    }
+}
